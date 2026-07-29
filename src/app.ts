36f904bb9d170be2fd46da5463910c13b50aa14f
dhruvnakum <dhruv.nakum25@gmail.com>
2026-07-28 21:12:23 -0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import config from "config";
+import express, { Request, Response } from "express";
+import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send(config.get("server.port"));
+});
+
+app.use(globalErrorHandler);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import config from "config";
+import app from "./app";
+import logger from "./config/logger";
+
+const startServer = () => {
+    const PORT: number = config.get("server.port");
+    try {
+        app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            logger.on("finish", () => {
+                process.exit(1);
+            });
+        }
+    }
+};
+
+startServer();
