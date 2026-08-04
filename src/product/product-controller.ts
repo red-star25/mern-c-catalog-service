a63faa71f6028b78c7290e60278585daee7492e6
dhruvnakum <dhruv.nakum25@gmail.com>
2026-08-04 16:00:25 -0700

    add create product

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -2,12 +2,42 @@ import { NextFunction, Response } from "express";
 import { Request } from "express-jwt";
 import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
+import { ProductService } from "./product-service";
+import { CreateProductBody, Product } from "./product-types";
 
 export class ProductController {
-    async create(req: Request, res: Response, next: NextFunction) {
+    constructor(private productService: ProductService) {}
+
+    create = async (req: Request, res: Response, next: NextFunction) => {
         const result = validationResult(req);
         if (!result.isEmpty()) {
             return next(createHttpError(400, result.array()[0].msg as string));
         }
-    }
+
+        const {
+            name,
+            description,
+            priceConfiguration,
+            attributes,
+            tenantId,
+            categoryId,
+            image,
+        } = req.body as CreateProductBody;
+
+        const product = {
+            name,
+            description,
+            priceConfiguration: JSON.parse(
+                priceConfiguration,
+            ) as Product["priceConfiguration"],
+            attributes: JSON.parse(attributes) as Product["attributes"],
+            tenantId,
+            categoryId,
+            image,
+        } as Product;
+
+        const newProduct = await this.productService.createProduct(product);
+
+        res.status(201).json({ id: newProduct._id });
+    };
 }
---
+++ src/product/product-types.ts
@@ -0,0 +1,29 @@
+export interface ProductPriceConfiguration {
+    priceType: "base" | "additional";
+    availableOptions: Record<string, number>;
+}
+
+export interface ProductAttribute {
+    name: string;
+    value: unknown;
+}
+
+export interface Product {
+    name: string;
+    description: string;
+    priceConfiguration: Record<string, ProductPriceConfiguration>;
+    attributes: ProductAttribute[];
+    tenantId: string;
+    categoryId: string;
+    image: string;
+}
+
+export interface CreateProductBody {
+    name: string;
+    description: string;
+    priceConfiguration: string;
+    attributes: string;
+    tenantId: string;
+    categoryId: string;
+    image: string;
+}
